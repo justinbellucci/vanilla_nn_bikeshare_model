d65f784a9b9122cca67761194990896e0d1dcbe1
justinbellucci <justin.bellucci@me.com>
2020-06-10 12:41:48 -0600

    add backpropagation functionality

--- vanilla_nn_class.py
+++ vanilla_nn_class.py
@@ -20,7 +20,7 @@ class VanillaNN(object):
         self.activation_fn = lambda x: 1 / (1 + np.exp(-x))    
 
     def forward(self, features):
-        """ Perfom a forward pass through the network.
+        """ Perform a forward pass through the network.
 
             Arguments:  
                 - features
@@ -37,3 +37,28 @@ class VanillaNN(object):
         final_outputs = final_inputs
 
         return final_outputs, hidden_outputs
+
+    def backprop(self, final_outputs, hidden_outputs, X, y, delta_weights_i_h, delta_weights_h_o):
+        """ Perform backpropagation to calculate gradients.
+
+            Arguments:
+                - final_ouputs (floats): output of network
+                - hidden_outputs (floats): hidden layer output
+                - X (model features)
+                - y (model targets)
+                - delta_weights_i_h (floats): change in weights from in to hidden
+                - delta_weights_h_o (floats): change in weights from hidden to out
+        """
+        # calculate error
+        error = y - final_outputs
+        # output error term is just the output error
+        output_error_term = error * 1.0
+        # calculate hidden error term
+        hidden_error = np.dot(self.weights_hidden_to_out, output_error_term)
+        hidden_error_term = hidden_error * hidden_outputs * (1 - hidden_outputs)
+        # caclulate the weight step (hidden to output)
+        delta_weights_h_o += hidden_outputs[:,None] * output_error_term
+        # caclulate the weight step (input to hidden)
+        delta_weights_i_h += X[:,None] * hidden_error_term
+
+        return delta_weights_i_h, delta_weights_h_o
